f9a347e34403b017ab60a960139283d11de39c4a
Angelica0715 <52756023+Angelica0715@users.noreply.github.com>
2020-12-07 09:59:52 -0800

    Create version2.py

---
+++ version2.py
@@ -0,0 +1,9 @@
+#Sorting Hat Version 2
+
+import random
+
+#Lists of houses
+houses=["Slytherin","Gryffindor","Hufflepuff","Ravenclaw"]
+
+#Pick a house at random from houses.
+print(random.choice(houses))
